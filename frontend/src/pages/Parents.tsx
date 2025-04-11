f4d3f439cbf83613f6adcf686702c9adbfe1b079
amitkumarsingh01 <aksmlibts@gmail.com>
2025-04-12 01:32:23 +0530

    removing non functional part

--- frontend/src/pages/Parents.tsx
+++ frontend/src/pages/Parents.tsx
@@ -952,7 +952,7 @@ const Parents: React.FC = () => {
                         </svg>
                         Start New Assessment
                       </motion.button>
-                      <motion.button
+                      {/* <motion.button
                         whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }}
                         className="px-8 py-3 bg-transparent border border-green-500/30 hover:border-green-400 text-white rounded-xl transition-colors duration-300 flex items-center"
@@ -962,7 +962,7 @@ const Parents: React.FC = () => {
                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                         </svg>
                         Download PDF Report
-                      </motion.button>
+                      </motion.button> */}
                     </div>
                   </div>
                 )}
